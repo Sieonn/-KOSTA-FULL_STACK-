768d2e2ad36624365a070cb116dc179efa9217b0
sieon <sieon.eve@gmail.com>
2024-01-29 20:30:26 +0900

    f

---
+++ _0126/src/Study.java
@@ -0,0 +1,8 @@
+public class Study {
+    public static void main(String[] args) {
+        int x =2;
+        int y = 5;
+        char c = 'A';
+        System.out.println(y >= 5 || x < 0 && x > 2);
+    }
+}
