08c6d4223d2817f7d13e5d72a00a52ca5220792e
Sieon <sieon.eve@gmail.com>
2024-01-30 16:19:14 +0900

    s

--- _0130/src/ConstuctorTest1.java
+++ _0130/src/ConstuctorTest1.java
@@ -21,6 +21,7 @@ public class ConstuctorTest1 {
         Member m1 = new Member();
         m1.num = 100;
         m1.name = "홍길동";
+        //클래스 생성시 생성자를 하나도 안만들면 기본생성자가 자동으로 만들어지는데 하나라도 만들면 기본생성자는 자동으로 만들어지지 않기때문에 기본 생성자를 만들어 줘야합니다.
 
         Member m2 = new Member(101, "김길동");
         System.out.println();
