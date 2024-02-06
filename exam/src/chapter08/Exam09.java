d3c9455f64e4c23a20ce99afbc6f708d9c90eedf
Sieon <sieon.eve@gmail.com>
2024-02-06 17:35:50 +0900

    ss

--- exam/src/chapter08/Exam09.java
+++ exam/src/chapter08/Exam09.java
@@ -1,7 +1,35 @@
 package chapter08;
 
+
+class UnspportedFuntionException extends RuntimeException {
+    private final int ERR_CODE;
+
+    UnspportedFuntionException(String message) {
+        super(message);
+        ERR_CODE = 100;
+    }
+
+    UnspportedFuntionException(String message, int errCode) {
+        super(message);
+        ERR_CODE = errCode;
+    }
+
+    public int getERR_CODE() {
+        return ERR_CODE;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[" + getERR_CODE() + "]" + super.getMessage();
+    }
+}
+
 public class Exam09 {
-    public static void main(String[] args) {
-        throw  new UnsupportedFuntionException("지원하지 않는 기능입니다.", 100);
+    public static void main(String[] args) throws Exception {
+        try {
+            throw new UnspportedFuntionException("지원하지 않는 기능입니다.", 100);
+        } catch (UnspportedFuntionException e) {
+            System.out.println(e.getERR_CODE());
+        }
     }
 }
