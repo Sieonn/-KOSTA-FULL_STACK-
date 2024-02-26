e4cbc3afda201b9cd94a737482491ac75fceff17
sieon <sieon.eve@gmail.com>
2024-02-26 19:27:44 +0900

    d

--- Java-start/src/array/ArrayDi3.java
+++ Java-start/src/array/ArrayDi3.java
@@ -1,14 +1,15 @@
 package array;
 
-public class ArrayDi1 {
+public class ArrayDi3 {
     public static void main(String[] args) {
         //2x3 2 차원 배열을 만든다.
         int[][] arr = new int[][]{
                 {1, 2, 3},
-                {4, 5, 6}}; //행은 2 열은 3
+                {4, 5, 6},
+                {7, 8, 9}}; //행은 2 열은 3
 
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 3; col++) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
 
                 System.out.print(arr[row][col] +
                         " ");
