e9fd34309d37da86d7a1c106b75be6e69a676232
sieon <sieon.eve@gmail.com>
2024-02-28 17:15:24 +0900

    Create ArrayEx5.java

---
+++ JAVA/Java-start/src/array/ArrayEx5.java
@@ -0,0 +1,12 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayEx5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("입력받을 숫자의 개수를 입력하세요.:");
+        int input = scanner.nextInt();
+
+    }
+}
