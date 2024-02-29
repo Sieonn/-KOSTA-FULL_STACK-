12a01117dc4fb908bf82b10c9b90e995a1d3f915
sieon <sieon.eve@gmail.com>
2024-02-29 09:46:04 +0900

    z

--- JAVA/Java-start/src/array/ArrayEx5.java
+++ JAVA/Java-start/src/array/ArrayEx5.java
@@ -7,6 +7,16 @@ public class ArrayEx5 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("입력받을 숫자의 개수를 입력하세요.:");
         int input = scanner.nextInt();
+        int sum =0;
+        int [] nums = new int[input];
+        for(int i=0; i<input; i++){
+            nums[i] = scanner.nextInt();
+            sum += nums[i];
+        }
+        double avg = (double) sum/nums.length;
+        System.out.println("입력한 정수의 합계:"+ sum);
+        System.out.println("입력한 정수의 평균: "+avg);
+
 
     }
 }
