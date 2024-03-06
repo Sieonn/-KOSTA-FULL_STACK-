78761dd3eecf0c48de4cb0a783c9ef5ee99b2835
sieon <sieon.eve@gmail.com>
2024-03-06 10:12:10 +0900

    ss

--- JAVA/BankProj/src/main/webapp/header.jsp
+++ JAVA/BankProj/src/main/webapp/header.jsp
@@ -1,21 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+   String user = (String) session.getAttribute("user");
+%>
 <style>
-	a {text-decoration:none; margin-left:10px; margin-right:10px;}
+   a {text-decoration:none; margin-left:10px; margin-right:10px;}
 </style>    
 <div style="width:100%;height:120px;background-color:navajowhite;padding:5px">
-	<h1 style="text-align:center"><i>kosta bank</i></h1>
-	<div style="float:left;">
-		<a href="makeaccount.jsp">계좌개설</a>
-		<a href="deposit.jsp">입금</a>
-		<a href="withdraw.jsp">출금</a>
-		<a href="accountinfoform.jsp">계좌조회</a>
-		<a href="allaccountinfo.jsp">전체계좌조회</a>
-		<a href="transfer.jsp">계좌이체</a>
-	</div>
-	<div style="float:right;">
-		<a href="makeaccount.jsp">회원가입</a>
-		<a href="makeaccount.jsp">로그인</a>
-	</div>
+   <h1 style="text-align:center"><i>kosta bank</i></h1>
+   <div style="float:left;">
+      <a href="makeaccount.jsp">계좌개설</a>
+      <a href="deposit.jsp">입금</a>
+      <a href="withdraw.jsp">출금</a>
+      <a href="accountinfoform.jsp">계좌조회</a>
+      <a href="allaccountinfo.jsp">전체계좌조회</a>
+      <a href="transfer.jsp">계좌이체</a>
+   </div>
+   <div style="float:right;">
+      <%if(user == null) {%>
+         <a href="login.jsp">로그인</a>
+      <%} else {%>      
+      <%=user %>님 환영합니다&nbsp;&nbsp;
+         <a href="logout">로그아웃</a>
+      <%} %>
+         <a href="join.jsp">회원가입</a>
+   </div>
 
 </div>    
---
+++ JAVA/Bankproj/src/main/java/servlet/Join.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Join
+ */
+@WebServlet("/Join")
+public class Join extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Join() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+
+		Member member = new Member(id, name, password, email, address);
+		HttpSession session = request.getSession();
+
+		// 중복 아이디 확인
+		Object obj = session.getAttribute(id);
+		if (obj != null) {
+			request.setAttribute("err", "사용중인 아이디입니다.");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("error.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			session.setAttribute(id, member);
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+}
