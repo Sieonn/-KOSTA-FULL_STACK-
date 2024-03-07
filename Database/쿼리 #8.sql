2387e78a14fc0cc0dfcc0e1d32ffec2fd11f1835
Sieon <sieon.eve@gmail.com>
2024-03-07 12:32:28 +0900

    s

---
+++ Database/쿼리 #8.sql
@@ -0,0 +1,10 @@
+-- ----------------------------------------------------------
+-- DML: insert, update, delete (데이터 삽입, 수정, 삭제)
+-- ---------------------------------------------------------
+-- insert into table_name (column_name1, cloumn_name2,...) values(value1, value2,...)
+
+CREATE TABLE USER (
+	id VARCHAR(10),
+	NAME VARCHAR(20)
+	);
+	
