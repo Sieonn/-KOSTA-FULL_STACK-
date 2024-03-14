ff6e5784c7b18390486bf51dc6fc0549f7fa75df
sieon <sieon.eve@gmail.com>
2024-03-14 12:31:25 +0900

    b

---
+++ JAVA/MybatisTest/src/main/java/dao/MemberDaoImpl.java
@@ -0,0 +1,20 @@
+package dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import dto.Member;
+import util.MybatisSqlSessionFactory;
+
+public class MemberDaoImpl implements MemberDao {
+	SqlSession sqlSession = MybatisSqlSessionFactory.getSqlSessionFactory().openSession();
+	@Override
+	public void insertMember(Member member) throws Exception {
+		sqlSession.insert("mapper.member.insertMember", member);
+		sqlSession.commit();
+	}
+
+	@Override
+	public Member selectMember(String id) throws Exception {
+		return sqlSession.selectOne("mapper.member.selectMember", id);
+	}
+}
